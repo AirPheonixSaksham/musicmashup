c5627c6bc4478f98475e593ef580849900a1485d
AirPheonixSaksham <166398246+AirPheonixSaksham@users.noreply.github.com>
2025-05-02 10:41:51 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+API_ID = int(getenv("API_ID", "21408627"))
+API_HASH = getenv("API_HASH", "465cca6fa782695983d49db306e0f8be")
+BOT_TOKEN = getenv("BOT_TOKEN", "7681079442:AAEEbh9RjLOGd_bIS0Jra5TeIDjE6jF_B0g")
